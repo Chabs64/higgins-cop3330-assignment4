04f2f92126d99a99d73c3cc9c41cad76af5274e6
chad <chadhiggins6@gmail.com>
2021-07-01 18:15:17 -0400

    more setup

---
+++ src/main/java/ucf/assignments/testMain.java
@@ -0,0 +1,62 @@
+package ucf.assignments;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+/*
+The application shall be able to manage at least 100 unique todo lists
+    - variable size list, structure
+
+A todo list shall have a title
+    - going to need a string title
+A todo list shall have the capacity to store at least 100 unique items
+    - variable size string list
+
+    - todo list class
+
+An item shall have a description
+An item shall have a due date, formatted as YYYY-MM-DD
+    - each items needs date string, and description string
+    - item class
+
+A user shall be able to add a new todo list
+    - new list button
+A user shall be able to remove an existing todo list
+    - delete list button
+A user shall be able to edit the title of an existing todo list
+    - change title button(just write over the title to reduce num of buttons)
+A user shall be able to add a new item to an existing todo list
+    - new item button
+A user shall be able to remove an item from an existing todo list
+    - delete item button
+A user shall be able to edit the description of an item within an existing todo list
+    - change description button(like title button, just write over)
+A user shall be able to edit the due date of an item within an existing todo list
+    - change date button
+A user shall be able to mark an item in a todo list as complete
+    - mark done button
+A user shall be able to display all of the existing items in a todo list
+    - display All button
+A user shall be able to display only the incompleted items in a todo list
+    - display incomplete button
+A user shall be able to display only the completed items in a todo list
+    -  display completed button
+
+A user shall be able to save all of the items in a single todo list to external storage
+    - save button
+A user shall be able to save all of the items across all of the todo lists to external storage
+    - save all button
+A user shall be able to load a single todo list that was previously saved to external storage
+    - load todo list button(prompt for tdlsit name)
+A user shall be able to load multiple todo lists that were previous saved to external storage
+    - load all button
+
+    15 buttons in total.
+ */
+
+public class testMain {
+
+
+}
