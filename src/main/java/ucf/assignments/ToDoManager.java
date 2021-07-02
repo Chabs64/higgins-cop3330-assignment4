39f9a2c2fbb1b8cee6c68f48a770aac775900b1b
chad <chadhiggins6@gmail.com>
2021-07-01 20:09:58 -0400

    more setup...

---
+++ src/main/java/ucf/assignments/ToDoManager.java
@@ -0,0 +1,41 @@
+ /*
+  *
+  *  *  UCF COP3330 Summer 2021 Assignment 4 Solution
+  *  *  Copyright 2021 Chad Higgins
+  *
+  */
+
+ package ucf.assignments;
+
+ import javafx.application.Application;
+ import javafx.fxml.FXMLLoader;
+ import javafx.scene.Parent;
+ import javafx.scene.Scene;
+ import javafx.stage.Stage;
+
+ import java.io.IOException;
+
+ public class ToDoManager extends Application {
+
+     public static void main(String[] args) {
+         launch(args);
+     }
+
+     @Override
+     public void start(Stage primaryStage) {
+
+         try {
+             Parent root = FXMLLoader.load(getClass().getResource("ToDoManager.fxml"));
+
+             Scene scene = new Scene(root);
+
+             primaryStage.setScene(scene);
+             primaryStage.setTitle("ToDoManager");
+             primaryStage.show();
+
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+
+     }
+ }
---
+++ src/main/java/ucf/assignments/ToDoManagerController.java
@@ -0,0 +1,12 @@
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  *  Copyright 2021 Chad Higgins
+ *
+ */
+
+package ucf.assignments;
+
+public class ToDoManagerController {
+
+}
